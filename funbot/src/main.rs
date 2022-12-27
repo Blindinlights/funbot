c099d529fc3e02b55786a8a83967ae74b007687e
Blindinlights <chenyutong007@gmail.com>
2022-12-27 20:22:00 +0800

    bilibili live reminder

--- funbot/src/main.rs
+++ funbot/src/main.rs
@@ -5,16 +5,19 @@ mod make_it_quote;
 mod openai;
 mod quote;
 mod weather;
+mod blive;
 use echo::{echo_msg, url_preview};
 use make_it_quote::quote_it;
 use openai::{open_image, open_journey};
 use quote::{bing_pic, copy_paste, one_quote};
 use rustqq::app::AsyncJobScheduler;
 use weather::{weather_query, weather_report};
+use blive::{add_live, delete_live};
 #[actix_web::main]
 async fn main() {
     let mut scheduler = AsyncJobScheduler::new();
     scheduler.add_job(festival::get_job());
+    scheduler.add_job(blive::blive_job());
     tokio::spawn(async move {
         loop {
             scheduler.run_pending().await;
@@ -31,6 +34,8 @@ async fn main() {
         .event(Box::new(quote_it))
         .event(Box::new(open_image))
         .event(Box::new(open_journey))
+        .event(Box::new(add_live))
+        .event(Box::new(delete_live))
         .run()
         .await
         .unwrap();
