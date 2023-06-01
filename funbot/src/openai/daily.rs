2edcc29c3e354b7ab24815d7002493983f04c787
Blindinlights <chenyutong007@gmail.com>
2023-06-01 15:15:07 +0800

    Update

--- funbot/src/openai/daily.rs
+++ funbot/src/openai/daily.rs
@@ -7,13 +7,13 @@ use rustqq::app::async_job::AsyncJob;
 use async_openai::types::{
     ChatCompletionRequestMessage as Chat, CreateChatCompletionRequestArgs as CreateChatArgs,
 };
-const POEM_API: &str = "https://v2.jinrishici.com/sentence";
+const POEM_API: &str = "https://v1.jinrishici.com/shanshui";
 const PROMPT: &str = "请帮我把这个句子 `{sentence}` 翻译成英语，请翻译的有诗意一点儿。";
 const DEFAULT_POEM: &str = "断虹霁雨，净秋空，山染修眉新绿。";
 async fn get_poem() -> Result<String, Box<dyn std::error::Error>> {
     let resp = reqwest::get(POEM_API).await?.text().await?;
     let poem: serde_json::Value = serde_json::from_str(&resp)?;
-    let poem = poem["data"]["content"].as_str().unwrap_or(DEFAULT_POEM);
+    let poem = poem["content"].as_str().unwrap_or(DEFAULT_POEM);
     Ok(poem.to_string())
 }
 async fn get_prompt(sentence: &str) -> Result<String, Box<dyn std::error::Error>> {
